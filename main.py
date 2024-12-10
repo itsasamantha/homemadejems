af209d3fab6977a9ef219fae404e3e5eb771a2b6
sam <spowell@brooklynsteamcenter.org>
2024-12-10 10:53:26 -0500

    starte connecting data base

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 from flask import Flask, render_template
+import pymysql
+from dynaconf import Dynaconf
 
 app = Flask(__name__)
+
+conf = Dynaconf(
+    settings_file = ["settings.toml"]
+)
+    
+def connect_db():
+    conn = pymysql.connect(
+        host = "10.100.34.80",
+        database = "spowell_home_made",
+        user = "spowell",
+        password = conf.password,
+        autocommit = True,
+        cursorclass = pymysql.cursors.DictCursor
+    )
+
+    return conn
 
 @app.route("/")
 def index():
     return render_template("homepage.html.jinja")
+
+@app.route("/browse")
+def product_browse():
+    conn = connect_db()
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM `Products`;")
+
+    results = cursor.fetchall()
+
+
+    return render_template("browse.html.jinja", products = results)
