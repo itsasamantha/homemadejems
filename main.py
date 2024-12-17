89fa240a1ff7dd47149a1b8be30e7f4c8df9a7d2
sam <spowell@brooklynsteamcenter.org>
2024-12-17 10:51:48 -0500

    sign_up pg

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 import pymysql
 from dynaconf import Dynaconf
+
 
 app = Flask(__name__)
 
 conf = Dynaconf(
     settings_file = ["settings.toml"]
 )
+
+app.secret_key = conf.secret_key
+
     
 def connect_db():
     conn = pymysql.connect(
@@ -35,6 +39,7 @@ def product_browse():
         cursor.execute("SELECT * FROM `Products`;")
     else:
         cursor.execute(f"SELECT * FROM `Products` WHERE `name` LIKE '%{query}%' OR `description` ;")
+        return render_template("browse.html.jinja", products = results)
 
     results = cursor.fetchall()
 
@@ -43,3 +48,51 @@ def product_browse():
 
 
     return render_template("browse.html.jinja", products = results)
+
+
+@app.route("/product")
+def product():
+    return render_template("product.html.jinja")
+
+@app.route("/sign_in")
+def sign_in():
+    return render_template("sign_in.html.jinja")
+
+@app.route("/sign_up", methods = ["POST","GET"])
+def sign_up():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+        address = request.form["address"]
+
+        if password != confirm_password:
+            flash("Your passwords do not match.")
+            return render_template("sign_up.html.jinja")
+        else:
+            if len(password) < 10:
+                flash("Your password should be at least 10 characters.")
+            else:
+                conn = connect_db()
+
+                cursor = conn.cursor()
+                try:
+                    cursor.execute(f""" 
+                    INSERT INTO `Customer` 
+                        (`first_name`, `last_name`,`email`,`password`, `address`)
+                        VALUES
+                            ('{first_name}', '{last_name}', '{email}', '{password}', '{address}');
+
+                    """)
+                except pymysql.err.IntegrityError:
+                    flash("There is already an account with this email.")
+                else:
+                    return redirect("/sign_in")
+                finally:
+                    cursor.close()
+                    conn.close()
+            
+
+    return render_template("sign_up.html.jinja")
