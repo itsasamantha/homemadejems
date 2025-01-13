a7913639e2a36e9339212da0f48e0df5b0865d36
sam <spowell@brooklynsteamcenter.org>
2025-01-13 10:52:01 -0500

    reviews nooooo

--- main.py
+++ main.py
@@ -23,11 +23,12 @@ class User:
     is_anonymous = False
     is_active = True
 
-    def __init__(self, user_id, email, first_name, last_name):
+    def __init__(self, user_id, email, first_name, last_name, address):
         self.id = user_id
         self.email = email
         self.first_name = first_name
         self.last_name = last_name
+        self.address = address
 
     def get_id(self):
         return str(self.id)
@@ -58,7 +59,7 @@ def load_user(user_id):
     conn.close()
 
     if result is not None:
-        return User(result["id"], result["email"], result["first_name"],result["last_name"])
+        return User(result["id"], result["email"], result["first_name"],result["last_name"],result["address"])
 
 
 @app.route("/")
@@ -89,8 +90,8 @@ def product_browse():
 @app.route("/product/<product_id>")
 def product(product_id):
     conn = connect_db()
-
     cursor = conn.cursor()
+
     cursor.execute(f"SELECT * FROM `Products` WHERE `id` = {product_id};")
 
     result = cursor.fetchone()
@@ -98,10 +99,14 @@ def product(product_id):
     if result is None:
         abort(404)
 
+    cursor.execute(f"SELECT * FROM `Review` JOIN `Customer` ON `Customer`.`id` = `customer_id` WHERE `id` = {product_id};")
+    review_result = cursor.fetchall()
+
+
     cursor.close()
     conn.close()
 
-    return render_template("product.html.jinja", product = result)
+    return render_template("product.html.jinja", product = result, reviews = review_result)
 
 @app.route("/product/<product_id>/cart", methods = ["POST"])
 @flask_login.login_required
@@ -230,33 +235,33 @@ def cart():
     return render_template("cart.html.jinja", products= results, total = total)
 
 
-@app.route("/cart/<cart_id>/delete", methods = ["POST"])
-def remove_product(cart_id):
+# @app.route("/cart/<cart_id>/delete", methods = ["POST"])
+# def remove_product(cart_id):
 
-    conn = connect_db()
-    cursor = conn.cursor()
+#     conn = connect_db()
+#     cursor = conn.cursor()
 
-    cursor.execute(f"DELETE FROM `Cart` WHERE `id` {cart_id};") 
+#     cursor.execute(f"DELETE FROM `Cart` WHERE `id` {cart_id};") 
     
-    cursor.close()
-    conn.close()
+#     cursor.close()
+#     conn.close()
 
-    return redirect("/cart")
+#     return redirect("/cart")
     
 
-@app.route("/cart/<cart_id>/update", methods = ["POST","GET"])
-def update_quantity(cart_id):
-    conn = connect_db()
-    cursor = conn.cursor()
+# @app.route("/cart/<cart_id>/update", methods = ["POST","GET"])
+# def update_quantity(cart_id):
+#     conn = connect_db()
+#     cursor = conn.cursor()
 
-    cursor.execute(f"""UPDATE `Cart`
-                       SET `quantity` = {new_quantity}
-    ;""") 
+#     cursor.execute(f"""UPDATE `Cart`
+#                        SET `quantity` = {new_quantity}
+#     ;""") 
 
-    cursor.close()
-    conn.close()
+#     cursor.close()
+#     conn.close()
 
-    return redirect("/cart")
+#     return redirect("/cart")
 
 
 @app.route("/checkout")
@@ -265,7 +270,7 @@ def checkout_page():
     cursor = conn.cursor()
 
     customer_id = flask_login.current_user.id
-
+    
     cursor.execute(f"""
                     SELECT `name`, `price`, `quantity`, `image`, `color`, `product_id`, `Cart`.`id`
                     FROM `Cart`
@@ -276,18 +281,87 @@ def checkout_page():
     results = cursor.fetchall()
 
     total = 0
+    count = 0
 
     for product in results:
         quantity = product["quantity"]
         price = product["price"]
         tot = quantity * price
         total = tot + total
+        count = count+1
+        
+    
 
 
     cursor.close()
     conn.close()
-    return render_template("checkout.html.jinja")
+    return render_template("checkout.html.jinja", products= results, total = total, count= count)
 
 @app.route("/check")
 def check():
     return render_template("check.html.jinja")
+
+
+@app.route("/thank_you")
+def thank_you():
+    return render_template("thankyou.html.jinja")
+
+@app.route("/sale", methods = ["POST","GET"])
+
+def sale():
+    if flask_login.current_user.is_authenticated:
+        conn = connect_db()
+        cursor = conn.cursor()
+        
+        customer_id = flask_login.current_user.id
+        status = "recieved"
+        name = request.form["name"]
+        card_num = request.form["card_num"]
+        expiration = request.form["expire"]
+        cvv = request.form["cvv"]
+
+        cursor.execute(f""" 
+                        INSERT INTO `Sale`
+                        (`customer_id`, `status`,`name_on_card`,`card_number`,`expiration`,`CVV`)
+                        VALUES
+                        ("{customer_id}","{status}","{name}","{card_num}","{expiration}","{cvv}");
+                        """)
+
+        sale_id = cursor.lastrowid
+
+        cursor.execute(f"SELECT * FROM `Cart` WHERE customer_id = '{customer_id}';")
+        
+        sale_result = cursor.fetchall()
+        
+        for sale in sale_result:
+            product = sale["product_id"]
+            qty = sale["quantity"]
+            cursor.execute (f"""
+                            INSERT INTO `SaleProduct` 
+                            (`sale_id`,`product_id`,`quantity`) 
+                            VALUES 
+                            ("{sale_id}","{product}","{qty}");
+                            """)
+        
+        cursor.execute (f"DELETE FROM `Cart` WHERE `customer_id` = '{customer_id}'")
+
+        return redirect("/thank_you")
+    else:
+        return redirect("/sign_in")
+
+@app.route("/product/<product_id>/product", methods = ["POST"])
+@flask_login.login_required
+def add_review(product_id):
+
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute(f""" 
+                    INSERT INTO `Cart`
+                        (`quantity`,`customer_id`,`product_id`)
+                    VALUES
+                        ("{quantity}","{customer_id}","{product_id}")
+                    ON DUPLICATE KEY UPDATE
+                        `quantity` = `quantity` + {quantity};
+                 """)
+    return None
